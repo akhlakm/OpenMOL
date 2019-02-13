9f22f92fb77486f43ac4e7a6835a82eec260ace6
akhlakm <me@akhlakm.com>
2019-02-13 01:08:39 -0500

    add amber_parm7 1st working version

---
+++ amber_parm7.py
@@ -0,0 +1,316 @@
+import openmol
+
+pointers = [
+	'NATOM', 	'NTYPES', 'NBONH',  'MBONA',  'NTHETH', 'MTHETA',
+	'NPHIH',    'MPHIA',  'NHPARM', 'NPARM',  'NNB',    'NRES',
+	'NBONA',    'NTHETA', 'NPHIA',  'NUMBND', 'NUMANG', 'NPTRA',
+	'NATYP',    'NPHB',   'IFPERT', 'NBPER',  'NGPER',  'NDPER',
+	'MBPER',    'MGPER',  'MDPER',  'IFBOX',  'NMXRS',  'IFCAP',
+	'NUMEXTRA', 'NCOPY'
+]
+
+def initialize():
+	MOL = openmol.initialize()
+	MOL['source_format'] = "AMBER PARM7"
+
+	MOL['parm_version_string'] = None
+	MOL['atom_type_index'] = []
+	MOL['atom_no_excluded'] = []
+
+	for i in pointers:
+		MOL['PARM_%s' %i] = 0
+
+	return MOL
+
+def process_last_section(MOL, section, lines, format):
+	items = []
+	for l in lines:
+		items += l.split()
+
+	if not section:
+		return True
+
+	if section == 'TITLE':
+		MOL['title'] = lines[0]
+
+	elif section == 'POINTERS':
+		if len(items) > len(pointers):
+			print('\nWarning: unknown PRMTOP pointer found. Ignoring ...', end=' ')
+
+		for i, v in enumerate(items):
+			if i < len(pointers):
+				MOL['PARM_%s' %pointers[i]] = int(v)
+
+		MOL['no_atoms'] = MOL['PARM_NATOM']
+		MOL['no_bonds'] = MOL['PARM_NBONA'] + MOL['PARM_NBONH']
+		MOL['no_angles'] = MOL['PARM_NTHETH'] + MOL['PARM_MTHETA']
+		MOL['no_dihedrals'] = MOL['PARM_NPHIH'] + MOL['PARM_MPHIA']
+		MOL['no_residues'] = MOL['PARM_NRES']
+		MOL['no_atom_types'] = MOL['PARM_NATYP']
+
+	elif section == 'ATOM_NAME':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and ATOM_NAME section mismatch')
+			return False
+
+		for name in items:
+			MOL['atom_name'].append(name)
+
+	elif section == 'CHARGE':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and CHARGE section mismatch')
+			return False
+
+		for q in items:
+			# in electionic units
+			MOL['atom_q'].append(float(q)/18.2223)
+
+	elif section == 'ATOMIC_NUMBER':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and ATOMIC_NUMBER section mismatch')
+			return False
+
+		for A in items:
+			MOL['atom_atomic_no'].append(int(A))
+
+	elif section == 'MASS':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and MASS section mismatch')
+			return False
+
+		for m in items:
+			MOL['atom_mass'].append(float(m))
+
+	elif section == 'ATOM_TYPE_INDEX':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and ATOM_TYPE_INDEX section mismatch')
+			return False
+
+		for t in items:
+			MOL['atom_type_index'].append(int(t) - 1)
+
+	elif section == 'NUMBER_EXCLUDED_ATOMS':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and NUMBER_EXCLUDED_ATOMS section mismatch')
+			return False
+
+		for ex in items:
+			MOL['atom_no_excluded'].append(int(ex))
+
+	elif section == 'RESIDUE_LABEL':
+		if len(items) != MOL['no_residues']:
+			print('Error: no_residues and RESIDUE_LABEL section mismatch')
+			return False
+
+		for i in items:
+			MOL['residue_name'].append(i)
+
+	elif section == 'RESIDUE_POINTER':
+		if len(items) != MOL['no_residues']:
+			print('Error: no_residues and RESIDUE_POINTER section mismatch')
+			return False
+
+		for i in items:
+			MOL['residue_start'].append(int(i) - 1)
+
+	elif section in ['BONDS_INC_HYDROGEN', 'BONDS_WITHOUT_HYDROGEN']:
+		for i in range(0, len(items), 3):
+			index0 = int(abs(int(items[i]))/3)
+			index1 = int(abs(int(items[i+1]))/3)
+			MOL['bond_from'].append(index0)
+			MOL['bond_to'].append(index1)
+
+	elif section in ['ANGLES_INC_HYDROGEN', 'ANGLES_WITHOUT_HYDROGEN']:
+		for i in range(0, len(items), 4):
+			index0 = int(abs(int(items[i]))/3)
+			index1 = int(abs(int(items[i+1]))/3)
+			index2 = int(abs(int(items[i+2]))/3)
+			MOL['angle_a'].append(index0)
+			MOL['angle_b'].append(index1)
+			MOL['angle_c'].append(index2)
+
+	elif section in ['DIHEDRALS_INC_HYDROGEN', 'DIHEDRALS_WITHOUT_HYDROGEN']:
+		for i in range(0, len(items), 5):
+			index0 = int(abs(int(items[i]))/3)
+			index1 = int(abs(int(items[i+1]))/3)
+			index2 = int(abs(int(items[i+2]))/3)
+			index3 = int(abs(int(items[i+3]))/3)
+			MOL['dihed_a'].append(index0)
+			MOL['dihed_b'].append(index1)
+			MOL['dihed_c'].append(index2)
+			MOL['dihed_d'].append(index3)
+
+	elif section == 'AMBER_ATOM_TYPE':
+		if len(items) != MOL['no_atoms']:
+			print('Error: no_atoms and AMBER_ATOM_TYPE section mismatch')
+			return False
+
+		for t in items:
+			MOL['atom_type'].append(t)
+
+		unique_atom_types = set(MOL['atom_type'])
+		MOL['unique_atom_types'] = list(unique_atom_types)
+
+	print('OK.')
+	return True
+
+def read_prmtop(prmtop):
+	global section_lines
+
+	MOL = initialize()
+
+	line_no = 0
+	section_line_no = 0
+	section = None
+	section_format = None
+	section_lines = []
+
+	for line in open(prmtop, 'r'):
+		line_no += 1
+		section_line_no += 1
+
+		line = line.strip()
+
+		if len(line) == 0:
+			continue
+
+		if line.startswith('%VERSION'):
+			parts = line.split()
+			if len(parts) < 2:
+				print('Invalid PRMTOP [line %d]:\n%s' %(line_no, line))
+				return None
+			else:
+				MOL['parm_version_string'] = ' '.join(parts[1:])
+
+		# new section
+		elif line.startswith('%FLAG'):
+			parts = line.split()
+			if len(parts) < 2:
+				print('Invalid PRMTOP [line %d]:\n%s' %(line_no, line))
+				return None
+			else:
+				if not process_last_section(MOL, section, section_lines, section_format):
+					return False
+
+				section = parts[1]
+				print('Reading %s ...' %section, end=' ')
+				section_line_no = 0
+				section_lines = []
+
+		elif line.startswith('%FORMAT'):
+			parts = line.split('(')
+			if len(parts) < 2:
+				print('Invalid PRMTOP [line %d]:\n%s' %(line_no, line))
+				return None
+			else:
+				section_format = parts[1][:-1]
+
+		else:
+			section_lines.append(line)
+
+	if not process_last_section(MOL, section, section_lines, section_format):
+		return False
+
+	print('Done.')
+	return MOL
+
+def read_rst7(MOL, rst_file):
+	line_no = 0
+	no_atoms = 0
+	items = []
+	last_line = None
+
+	for line in open(rst_file, 'r'):
+		line_no += 1
+		line = line.strip()
+
+		if len(line) == 0:
+			continue
+
+		last_line = line
+
+		if line_no == 1:
+			rst_title = line
+			continue
+		elif line_no == 2:
+			parts = line.split()
+			no_atoms = int(parts[0])
+			if len(parts) > 1:
+				MOL['time'] = float(parts[1])
+			if len(parts) > 2:
+				MOL['temp'] = float(parts[2])
+
+			if no_atoms != MOL['no_atoms']:
+				print('Error: RST7 no_atoms mismatch.')
+				return False
+		else:
+			items += line.split()
+
+	if len(items) < no_atoms * 3:
+		print('Error: RST7 no_atoms, coordinate items mismatch.')
+		return False
+
+	print('Reading coordinates ...', end=' ')
+
+	for i in range(0, no_atoms*3, 3):
+		MOL['atom_x'].append(float(items[i]))
+		MOL['atom_y'].append(float(items[i+1]))
+		MOL['atom_z'].append(float(items[i+2]))
+
+	print('OK.')
+
+	if len(items) >= no_atoms*6:
+		print('Reading velocities ...', end=' ')
+
+		for i in range(no_atoms*3, no_atoms*6, 3):
+			MOL['atom_vx'].append(float(items[i]))
+			MOL['atom_vy'].append(float(items[i+1]))
+			MOL['atom_vz'].append(float(items[i+2]))
+
+		print('OK.')
+
+	box_size_conditions = [
+		len(items) == no_atoms*3 + 3,
+		len(items) == no_atoms*6 + 3,
+		len(items) == no_atoms*3 + 6,
+		len(items) == no_atoms*6 + 6,
+	]
+
+	if any(box_size_conditions):
+		print('Reading box dimension ...', end=' ')
+		parts = last_line.split()
+		MOL['box_x'] = float(parts[0])
+		MOL['box_y'] = float(parts[1])
+		MOL['box_z'] = float(parts[2])
+
+		print('OK.')
+	else:
+		print('No PBC box information found.')
+
+	box_angle_conditions = [
+		len(items) == no_atoms*3 + 6,
+		len(items) == no_atoms*6 + 6,
+	]
+
+	if any(box_angle_conditions):
+		print('Reading box angles ...', end=' ')
+		parts = last_line.split()
+		MOL['box_alpha'] = float(parts[3])
+		MOL['box_beta'] = float(parts[4])
+		MOL['box_gamma'] = float(parts[5])
+
+		print('OK.')
+
+	print('Done.')
+	return MOL
+
+def read(prmtop, rst7):
+	MOL = read_prmtop(prmtop)
+	if not MOL:
+		return False
+	
+	MOL = read_rst7(MOL, rst7)
+	if not openmol.check(MOL):
+		return False
+
+	return MOL
