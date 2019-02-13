da932dd57b344691ce8b883572d1715e7cd02cda
akhlakm <me@akhlakm.com>
2019-02-12 21:10:25 -0500

    Initial Commit

---
+++ openmol.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import json
+
+def initialize():
+	global MOL
+
+	MOL = {}
+
+	MOL['title'] = None
+	MOL['description'] = ""
+	MOL['type'] = None
+	MOL['charge_type'] = None
+
+	MOL['no_atoms'] = None
+	MOL['no_bonds'] = None
+	MOL['no_atom_types'] = None
+	MOL['no_residues'] = None
+	MOL['no_angles'] = None
+	MOL['no_dihedrals'] = None
+	MOL['unique_atom_types'] = []
+
+	MOL['atom_name'] = []
+	MOL['atom_x'] = []
+	MOL['atom_y'] = []
+	MOL['atom_z'] = []
+	MOL['atom_q'] = []
+	MOL['atom_type'] = []
+	MOL['atom_resname'] = []
+	MOL['atom_resid'] = []
+	MOL['atom_mass'] = []
+	MOL['atom_atomic_no'] = []
+
+	MOL['bond_from'] = []
+	MOL['bond_to'] = []
+	MOL['bond_type'] = []
+
+	MOL['angle_a'] = []
+	MOL['angle_b'] = []
+	MOL['angle_c'] = []
+
+	MOL['residue_name'] = []
+	MOL['residue_start'] = []
+	MOL['residue_end'] = []
+	MOL['residue_type'] = []
+
+	return MOL
+
+
+def write_json(MOL, json_file, compress=False):
+	with open(json_file, 'w+') as fp:
+		if compress:
+			json.dump(MOL,fp, indent=None, separators=(',', ':'))
+		else:
+			json.dump(MOL, fp, indent=4)
+
+	print('%s written' %json_file)
+
+
+def load_json(json_file):
+	with open(json_file, 'r') as fp:
+		MOL = json.load(fp)
+
+	MOL['json_file'] = json_file
+	print('%s loaded' %json_file)
+
+	return MOL
+
+
+def check_atoms_ok(MOL):
+	conditions_fail = [
+		not MOL['no_atoms'],
+		len(MOL['atom_q']) and MOL['no_atoms'] != len(MOL['atom_y']),
+	]
+
+	conditions_ok = [
+		MOL['no_atoms'] == len(MOL['atom_name']),
+		MOL['no_atoms'] == len(MOL['atom_x']),
+		MOL['no_atoms'] == len(MOL['atom_y']),
+		MOL['no_atoms'] == len(MOL['atom_z']),
+		MOL['no_atoms'] == len(MOL['atom_type']),
+	]
+
+	if any(conditions_fail) or not all(conditions_ok):
+		print('Error: Summary and atom attribute list mismatch.')
+		return False
+
+	return True
+
+def check_bonds_ok(MOL):
+	conditions_fail = [
+		MOL['no_bonds'] == None and len(MOL['bond_from']) > 0,
+	]
+
+	conditions_ok = [
+		MOL['no_bonds'] == len(MOL['bond_from']),
+		MOL['no_bonds'] == len(MOL['bond_type']),
+		MOL['no_bonds'] == len(MOL['bond_to']),
+	]
+
+	if any(conditions_fail) or not all(conditions_ok):
+		print('Error: Summary and bond attribute list mismatch.')
+		return False
+
+	return True
+
+def check_residues_ok(MOL):
+	conditions_fail = [
+		MOL['no_residues'] == None and len(MOL['residue_name']) > 0,
+	]
+
+	conditions_ok = [
+		MOL['no_residues'] == len(MOL['residue_name']),
+		MOL['no_residues'] == len(MOL['residue_start']),
+	]
+
+	if any(conditions_fail) or not all(conditions_ok):
+		print('Error: Summary and residue attribute list mismatch.')
+		return False
+
+	return True
+
+
+def check(MOL):
+	return check_atoms_ok(MOL) and check_bonds_ok(MOL) and check_residues_ok(MOL)
+
+def update_summary(MOL, overwrite=False):
+	if overwrite or MOL['no_atoms'] is None:
+		MOL['no_atoms'] = len(MOL['atom_x'])
+
+	if overwrite or MOL['no_bonds'] is None:
+		MOL['no_bonds'] = len(MOL['bond_from'])
+
+	if overwrite or MOL['no_residues'] is None:
+		MOL['no_residues'] = len(MOL['residue_name'])
+
+	if overwrite or MOL['no_atom_types'] is None:
+		MOL['unique_atom_types'] = list(set(MOL['atom_type']))
+		MOL['no_atom_types'] = len(MOL['unique_atom_types'])
---
+++ tripos_mol2.py
@@ -0,0 +1,253 @@
+import openmol
+
+def initialize():
+	MOL = openmol.initialize()
+
+	MOL['source_format'] = 'TRIPOS MOL2'
+
+	MOL['atom_status_bit'] = []
+	MOL['residue_dict_type'] = []
+	MOL['residue_chain'] = []
+	MOL['residue_sub_type'] = []
+	MOL['residue_comment'] = []
+	MOL['residue_inter_bonds'] = []
+
+	return MOL
+
+def read(mol2_file):
+	MOL = initialize()
+
+	line_no = 0
+	section_line_no = 0
+	section = None
+
+	# variables for sanity checking
+	last_section = None
+	type_ok = False
+	charge_ok = False
+	name_ok = False
+	summary_ok = False
+
+
+	for line in open(mol2_file, 'r'):
+
+		line_no += 1
+		section_line_no += 1
+
+		line = line.strip()
+
+		if len(line) == 0:
+			continue
+
+		# comments
+		if line.startswith('#'):
+			MOL['description'] += "%s\n" %line
+			continue
+
+		# new section definition
+		if line.startswith('@<'):
+			parts = line.split('>')
+
+			if len(parts) < 2:
+				print('Invalid MOL2 [line %d]:\n%s' %(line_no, line))
+				return None
+			else:
+				last_section = section
+
+				# check before jumping into new section
+
+				if last_section == 'MOLECULE':
+					print('OK.')
+
+				if last_section == 'ATOM':
+					if not openmol.check_atoms_ok(MOL):
+						return None
+					else:
+						unique_atom_types = set(MOL['atom_type'])
+						MOL['unique_atom_types'] = list(unique_atom_types)
+						print('OK.')
+
+				if last_section == 'BOND':
+					if not openmol.check_bonds_ok(MOL):
+						return None
+					else:
+						print('OK.')
+
+				if last_section == 'SUBSTRUCTURE':
+					if not openmol.check_residues_ok(MOL):
+						return None
+					else:
+						print('OK.')
+
+				section = parts[1]
+				print('Reading %s ...' %section, end=' ')
+				section_line_no = 0
+				continue
+
+		# handle sections
+		if section == 'MOLECULE':
+			if section_line_no == 1:
+				MOL['title'] = line 
+				name_ok = True
+
+			elif section_line_no == 2:
+				parts = line.split()
+				if len(parts) > 0:
+					MOL['no_atoms'] = int(parts[0])
+
+				if len(parts) > 1:
+					MOL['no_bonds'] = int(parts[1])
+
+				if len(parts) > 2:
+					MOL['no_residues'] = int(parts[2])
+
+				if len(parts) > 3:
+					MOL['no_features'] = int(parts[3])
+
+				if len(parts) > 4:
+					MOL['no_sets'] = int(parts[4])
+
+				summary_ok = True
+
+			elif section_line_no == 3:
+				MOL['type'] = line 
+				type_ok = True
+
+			elif section_line_no == 4:
+				MOL['charge_type'] = line
+				charge_ok = True
+
+			elif section_line_no == 5:
+				MOL['mol2_status_bits'] = line
+
+			elif section_line_no == 6:
+				MOL['mol2_comment'] = line
+
+		elif section == 'ATOM':
+			parts = line.split()
+
+			if len(parts) < 6:
+				print('Invalid MOL2 [line %d]:\n%s' %(line_no, line))
+				return None
+
+			MOL['atom_name'].append(parts[1])
+			MOL['atom_x'].append(float(parts[2]))
+			MOL['atom_y'].append(float(parts[3]))
+			MOL['atom_z'].append(float(parts[4]))
+			MOL['atom_type'].append(parts[5])
+
+			if len(parts) > 6:
+				MOL['atom_resid'].append(int(parts[6]) - 1)
+
+			if len(parts) > 7:
+				MOL['atom_resname'].append(parts[7])
+
+			if len(parts) > 8:
+				MOL['atom_q'].append(float(parts[8]))
+
+			if len(parts) > 9:
+				MOL['atom_status_bit'].append(parts[9])
+
+
+		elif section == 'BOND':
+			parts = line.split()
+
+			if len(parts) < 4:
+				print('Invalid MOL2 [line %d]:\n%s' %(line_no, line))
+				return None
+
+			MOL['bond_from'].append(int(parts[1]) - 1)
+			MOL['bond_to'].append(int(parts[2]) - 1)
+			MOL['bond_type'].append(parts[3])
+
+			if len(parts) > 4:
+				MOL['bond_status_bit'].append(parts[4])
+
+		elif section == 'SUBSTRUCTURE':
+			parts = line.split()
+
+			if len(parts) < 3:
+				print('Invalid MOL2 [line %d]:\n%s' %(line_no, line))
+				return None
+
+			MOL['residue_name'].append(parts[1])
+			MOL['residue_start'].append(int(parts[2]) - 1)
+
+			if len(parts) > 3:
+				MOL['residue_type'].append(parts[3])
+			if len(parts) > 4:
+				MOL['residue_dict_type'].append(parts[3])
+			if len(parts) > 5:
+				MOL['residue_chain'].append(parts[3])
+			if len(parts) > 6:
+				MOL['residue_sub_type'].append(parts[3])
+			if len(parts) > 7:
+				MOL['residue_inter_bonds'].append(parts[3])
+			if len(parts) > 8:
+				MOL['residue_status_bits'].append(parts[3])
+			if len(parts) > 9:
+				MOL['residue_comment'].append(parts[3])
+
+		else:
+			# unknown section
+			# silence is golden
+			pass
+
+	print('Done.')
+	return MOL
+
+def write(MOL, mol2_file):
+	fp = open(mol2_file, 'w+')
+	fp.write('@<TRIPOS>MOLECULE\n')
+	molecstr = 	"{title}\n" \
+				"{no_atoms:>5d} {no_bonds:>5d} {no_residues:>5d}\n" \
+				"{type}\n" \
+				"{charge_type}\n\n"
+
+	fp.write(molecstr.format(**MOL))
+
+	fp.write('@<TRIPOS>ATOM\n')
+
+	for i in range(MOL['no_atoms']):
+		resid = MOL['atom_resid'][i]
+		atom = {
+			'id' : i + 1,
+			'name': MOL['atom_name'][i],
+			'x': MOL['atom_x'][i],
+			'y': MOL['atom_y'][i],
+			'z': MOL['atom_z'][i],
+			'type': MOL['atom_type'][i],
+			'resid': MOL['atom_resid'][i] + 1,
+			'resname': MOL['residue_name'][resid],
+			'charge': MOL['atom_q'][i]
+		}
+		atomstr =	"{id:>7d} {name:<5}  " \
+					"{x:>7.4f}  {y:>7.4f}  {z:>7.4f}   {type:>3} " \
+					"{resid:>3} {resname:<5}   {charge:>11.6f}\n"
+		fp.write(atomstr.format(**atom))
+
+	fp.write('@<TRIPOS>BOND\n')
+	for i in range(MOL['no_bonds']):
+		bond = {
+			'id' : i + 1,
+			'from': MOL['bond_from'][i] + 1,
+			'to': MOL['bond_to'][i] + 1,
+			'type': MOL['bond_type'][i],
+		}
+		bondstr =	"{id:>7d}  {from:>7d}  {to:>7d}   {type:>3} \n"
+		fp.write(bondstr.format(**bond))
+
+	fp.write('@<TRIPOS>SUBSTRUCTURE\n')
+	for i in range(MOL['no_residues']):
+		resid = MOL['atom_resid'][i]
+		subst = {
+			'id' : i + 1,
+			'name': MOL['residue_name'][i],
+			'root': MOL['residue_start'][i] + 1,
+			'type': MOL['residue_type'][i],
+		}
+		resstr =	"{id:>7d}  {name:>7}  {root:>7d}   {type:>7} \n"
+		fp.write(resstr.format(**subst))
+
+	fp.close()
+	print('%s written.' %mol2_file)
